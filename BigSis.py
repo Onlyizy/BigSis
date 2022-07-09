407fa9564db915fe9cfbb127fab82cca28f4d620
Onlyizy <kmorelisaac@gmail.com>
2022-07-09 05:42:02 -0400

    Write correct prompt and debug config file writer

--- BigSis.py
+++ BigSis.py
@@ -1,30 +1,50 @@
+from csv import DictReader, DictWriter
 from datetime import datetime
-import os
+from os import listdir
+from platform import system
 
 VERSION=1.0
 PSEUDO_PRIVACY_DISCLAIMER="disclaimer"
+FIRST_TIME_RUN_DISCLAIMER="disclaimer1"
+params=["runmode","pseudo","chores"]
+
+def config_file(disclamers,params):
+    """Returns a tuple with relevant data for next operations in this order runmode, pseudo, chores"""
+    FIRST_TIME_RUN_DISCLAIMER,PSEUDO_PRIVACY_DISCLAIMER=disclamers
+
+    first=True
+
+    if "config.csv" in listdir():
+
+        first=False
+
+    with open("config.csv","a+",newline='') as f:
+
+        if first==True:
+
+            print(FIRST_TIME_RUN_DISCLAIMER)
+            runmode=input("(A)dvanced or (M)inimal: ")
+            Rlist=[]
+            Clist=[]
+            Rnum=int(input("How much Roommates?"))
+            print(PSEUDO_PRIVACY_DISCLAIMER)
+            Rlist=set(map(lambda a: input("Pseudo"),range(Rnum)))
+            Cnum=int(input("How many chores? "))
+            Clist=set(map(lambda a: input("Chores"),range(Cnum)))
+            DataWriter=DictWriter(f,fieldnames=params,extrasaction="raise",restval="")
+            DataWriter.writeheader()
+            DataWriter.writerow({"runmode":runmode, "pseudo":Rlist, "chores":Clist})
+        
+        DataReader=DictReader(f,restkey="",restval="")
+        if 
+        t=list(DataReader)[-1]["runmode"],list(DataReader)[-1]["pseudo"],list(DataReader)[-1]["chores"]
+    return t
 
 def logwriter()->None:
-    first=True
-    if "log.txt" in os.listdir(os.curdir):
-        first=False
-    f=open("log.txt","a+")
-    f.writelines(VERSION, datetime.now)
-    if first==True:
-        runmode=input("Advanced or Minimal: ")
-        f.write(runmode)
-
-        Rlist=[]
-        Clist=[]
-
-        Rnum=int(input("How much Roommates?"))
-        print(PSEUDO_PRIVACY_DISCLAIMER)
-        Rlist=list(map(lambda a: input("Pseudo"),range(Rnum)))
-        Cnum=int(input("How many chores? "))
-        Clist=list(map(lambda a: input("Chores"),range(Cnum)))
-        f.writelines(Rlist,Clist)
-    f.close
-
-def dispatcher(runmode: str)-> dict:
-    
-
+    """Write useful info about the program while running. intended to be useful for debugging
+    """
+    with open("log.txt","a+") as f:
+        f.writelines([VERSION, datetime.now,system(),])
+            
+t=config_file((FIRST_TIME_RUN_DISCLAIMER, PSEUDO_PRIVACY_DISCLAIMER),params)
+logwriter()
